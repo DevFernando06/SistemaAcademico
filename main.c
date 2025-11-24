e667e5ba09855cff3d07553cedcd0758fb927d1c
DevFernando06 <srnando4312@gmail.com>
2025-11-24 13:41:57 -0300

    fix: error de sintax no default

--- main.c
+++ main.c
@@ -192,7 +192,7 @@ int main() {
             case '4': exibirD(); break;
             case '5': atribuirNota(); break;
             case '6': exibirTudo(); break;
-            default;
+            default:
             continue;
         }
 
